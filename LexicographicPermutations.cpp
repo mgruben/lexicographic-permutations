d7c049adf2d1b2468d53b6d6987f3d01b33d8f58
mgruben <GrubenM@gmail.com>
2016-09-17 13:55:12 -0500

    Implement factorial helper

--- LexicographicPermutations.cpp
+++ LexicographicPermutations.cpp
@@ -2,7 +2,21 @@
 #include <vector>
 using namespace std;
 
+int factorial(int n) {
+    if (n == 1 || n == 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+int indexPermutation(vector<int> perms, int index) {
+    int ans = 0, r;
+    do {
+        
+    } while (r != 0);
+    return ans;
+}
+
 int main() {
     vector<int> perms {0, 1, 2};
+    cout << factorial(10) << endl;
     return 0;
 }
