c5ebb48c3e75203d3d4c66a2bb525745c29db7c0
mgruben <GrubenM@gmail.com>
2016-09-17 14:05:45 -0500

    Implement string-based permuter

--- LexicographicPermutations.cpp
+++ LexicographicPermutations.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int factorial(int n) {
@@ -7,16 +8,28 @@ int factorial(int n) {
     else return n * factorial(n - 1);
 }
 
-int indexPermutation(vector<int> perms, int index) {
-    int ans = 0, r;
+string indexPermutation(vector<int> perms, int index) {
+    index--;
+    string ans = "";
+    int r = index;
+    int f = perms.size() - 1;
     do {
-        
+        int thisFactorial = factorial(f--);
+        int thisIndex = r / thisFactorial;
+        ans += perms[thisIndex] + 48;
+        perms.erase(perms.begin() + thisIndex);
+        r %= thisFactorial;
     } while (r != 0);
+    while (perms.size() > 0) {
+        ans += perms[0] + 48;
+        perms.erase(perms.begin());
+    }
     return ans;
 }
 
 int main() {
     vector<int> perms {0, 1, 2};
-    cout << factorial(10) << endl;
+    for (int i = 1; i <= 6; i++)
+    cout << indexPermutation(perms, i) << endl;
     return 0;
 }
